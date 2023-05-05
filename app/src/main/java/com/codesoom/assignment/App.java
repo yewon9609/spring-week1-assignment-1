0cc951f0ba9401965edd6bc1d579e1efd47cff4f
yewon.seo <dodnjs1241@naver.com>
2023-05-05 23:07:04 +0900

    feat: HttpServer설정 추가

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,23 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.codesoom.assignment.handler.DemoHttpHandler;
+import com.codesoom.assignment.repositroy.TaskRepository;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
-    public static void main(String[] args) {
+public class App {
+
+  public static void main(String[] args) {
+    InetSocketAddress address = new InetSocketAddress("localhost", 8000);
+    try {
+      HttpServer httpServer = HttpServer.create(address, 0);
+      HttpHandler handler = new DemoHttpHandler(new TaskRepository());
+      httpServer.createContext("/", handler);
+      httpServer.start();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
